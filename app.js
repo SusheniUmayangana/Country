d353f99a932616cd90ae6cd79ff1d1ec2f825660
SusheniUmayangana <umayangiks@gmail.com>
2024-07-07 12:29:23 +0530

    search

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let tbCountries = document.getElementById("tblCountries");
 
-let tableBody = `      <tr>
+let tableBody = ` <tr>
             <th>Name</th>
             <th>Falg</th>
         </tr>` ;
@@ -28,3 +28,28 @@ fetch("https://restcountries.com/v3.1/all")
     tblCountries.innerHTML=tableBody;
 
 })
+
+function searchCountry(){
+    let userInput = document.getElementById("txtInput").value;
+
+    let flagImg = document.getElementById("flagImg");
+    let name = document.getElementById("name");
+    let officialName = document.getElementById("officialName");
+    let region = document.getElementById("region");
+    let population = document.getElementById("population");
+
+    fetch(`https://restcountries.com/v3.1/name/${userInput}`)
+    .then(res=>res.json())
+    .then(data=>{
+        data.forEach(obj=>{
+            console.log(obj);
+            flagImg.src=obj.flags.png
+            name.innerText=obj.name.common
+            officialName.innerText=obj.name.official
+
+            region.innerText=obj.region;
+            population.innerText=obj.population
+        })
+
+    })
+}
